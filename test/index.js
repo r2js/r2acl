8d3c618795165709b467982918cc641ffba786bc
Selcuk Fatih Sevinc <airsfs@gmail.com>
2017-07-21 17:13:16 +0300

    v1.0.2
    * test update

--- test/index.js
+++ test/index.js
@@ -5,15 +5,34 @@ const r2acl = require('../index');
 
 const expect = chai.expect;
 
-const app = r2base({ baseDir: __dirname });
+const app = r2base();
 app.start()
-  .serve(r2mongoose, { database: 'r2test' })
+  .serve(r2mongoose, { database: 'r2acl' })
   .serve(r2acl)
   .into(app);
+
+const Acl = app.service('Acl');
+
+before((done) => {
+  // wait for connection
+  app.service('Mongoose').connection.on('open', () => {
+    done();
+  });
+});
 
 describe('r2acl', () => {
   it('should create acl service', () => {
     expect(app.services.Acl).to.not.equal(undefined);
+  });
+
+  it('should allow to resource', (done) => {
+    Acl.allow('guest', 'blogs', 'view')
+      .then(() => Acl.addUserRoles('guest', 'guest'))
+      .then(() => Acl.allowedPermissions('guest', 'blogs'))
+      .then((data) => {
+        expect(data.blogs).to.deep.equal(['view']);
+        done();
+      });
   });
 });
 
